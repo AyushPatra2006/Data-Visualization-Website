1f6521ae16f2ff7d2c9496d3c25e65996eb038af
Ayush Patra <apatra8@wisc.edu>
2025-07-15 19:18:23 +0000

    36% completed

--- projects/mp5/main.py
+++ projects/mp5/main.py
@@ -7,8 +7,42 @@ If you're working alone, enter `None` for the partner fields.
 
 '''
 Project: MP5
-Student 1: <Name>, <NETID>
+Student 1: Ayush Patra, apatra8
 Student 2: <Name>, <NETID>
 Student 3: <Name>, <NETID>
 Student 4: <Name>, <NETID>
 '''
+import pandas as pd
+from flask import Flask, request, jsonify
+
+# TODO: Add a comment about your data source
+
+app = Flask(__name__)
+df = pd.read_csv("main.csv")
+
+@app.route('/')
+def home():
+    with open("index.html") as f:
+        html = f.read()
+
+    return html
+
+@app.route('/browse.html')
+def browse():
+    with open("browse.html") as f:
+        html = f.read()
+
+    return html
+@app.route('/donate.html')
+def donate():
+    with open("donate.html") as f:
+        html = f.read()
+
+    return html
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", debug=True, threaded=False) # don't change this line!
+
+# NOTE: app.run never returns (it runs for ever, unless you kill the process)
+# Thus, don't define any functions after the app.run call, because it will
+# never get that far.
